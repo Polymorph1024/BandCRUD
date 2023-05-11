8726165669f738f2ad01130107113f9b4caea753
Polymorph1024 <denisandreev93@gmail.com>
2023-05-12 00:13:50 +0300

    added controller and html-pages

--- src/main/java/ru/ad/bandcrud/BandCRUD/controller/BandController.java
+++ src/main/java/ru/ad/bandcrud/BandCRUD/controller/BandController.java
@@ -1,7 +1,11 @@
 package ru.ad.bandcrud.BandCRUD.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.ad.bandcrud.BandCRUD.model.Band;
 import ru.ad.bandcrud.BandCRUD.service.BandService;
 
@@ -9,10 +13,48 @@ import java.util.List;
 
 @Controller
 class BandController {
-    private BandService bandService;
-    @GetMapping("/band")
-    public String findAll(){
+
+    private final BandService bandService;
+
+    @Autowired
+    BandController(BandService bandService) {
+        this.bandService = bandService;
+    }
+
+    @GetMapping("/bands")
+    public String findAll(Model model) {
         List<Band> bands = bandService.findAll();
-        return "";
+        model.addAttribute("bands", bands);
+        return "band-list";
+    }
+
+    @GetMapping("/band-create")
+    public String createBandForm(Band band) {
+        return "band-create";
+    }
+
+    @PostMapping("/band-create")
+    public String createBand(Band band) {
+        bandService.saveBand(band);
+        return "redirect:/bands";
+    }
+
+    @GetMapping("/band-delete/{id}")
+    public String deleteBand(@PathVariable("id") Long id) {
+        bandService.deleteById(id);
+        return "redirect:/bands";
+    }
+
+    @GetMapping("/band-update/{id}")
+    public String updateBandForm(@PathVariable("id") Long id, Model model) {
+        Band band = bandService.findById(id);
+        model.addAttribute("band", band);
+        return "band-update";
+    }
+
+    @PostMapping("/band-update")
+    public String updateBand(Band band) {
+        bandService.saveBand(band);
+        return "redirect:/bands";
     }
 }
